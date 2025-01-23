5b6d7c182df273733b0ed73bc723182c059d1285
Jude Rozario <juderozario@juderozario.local>
2025-01-23 08:56:41 -0500

    fixing types

---
+++ model/model.ts
@@ -0,0 +1,14 @@
+export enum Roles {
+    admin = 0,
+    coding_admin = 1,
+    builder_admin = 2,
+    student_coder_lead = 3,
+    student_builder_lead = 4,
+    student_coder = 5,
+    student_builder = 6,
+    __LENGTH
+}
+
+// TODO: Add all the features as we go along
+export const Features = {
+}
